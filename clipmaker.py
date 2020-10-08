37d70cbac0b5ce4592da4e2dfa3b5d43c9f1e671
Nikolai_P <vlrmrgls@gmail.com>
2020-10-09 01:53:26 +0300

    Add both execution and update in light

--- clipmaker.py
+++ clipmaker.py
@@ -1,20 +1,26 @@
-import cv2
-import numpy as np
 import glob
- 
-img_array = []
 
-for filename in glob.glob('60test/*.png'):
-	img = cv2.imread(filename)
-	print(filename)
-	img_array.append(img)
-height, width, layers = img_array[1].shape
+import cv2
 
-size = (width,height) 
 
-out = cv2.VideoWriter('60test.avi', cv2.VideoWriter_fourcc(*'DIVX'), 60, (width,height))
- 
-for i in range(len(img_array)):
-	out.write(img_array[i])
+def make_clip():
+    img_array = []
 
-out.release()
+    for filename in glob.glob('60test/*.png'):
+        img = cv2.imread(filename)
+        print(filename)
+        img_array.append(img)
+    height, width, layers = img_array[1].shape
+
+    size = (width, height)
+
+    out = cv2.VideoWriter('60test.avi', cv2.VideoWriter_fourcc(*'DIVX'), 60, (width, height))
+
+    for i in range(len(img_array)):
+        out.write(img_array[i])
+
+    out.release()
+
+
+if __name__ == '__main__':
+    make_clip()
---
+++ immitation_mp3.py
@@ -0,0 +1,6 @@
+import gen_3
+import clipmaker
+
+gen_3.create_clip(60, 30, 10, "60test", gen_3.velocities, gen_3.turn_rates, gen_3.radiusesX, gen_3.radiusesY, 5)
+
+clipmaker.make_clip()
